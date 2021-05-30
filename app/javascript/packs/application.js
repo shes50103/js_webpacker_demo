9d627e7cc4cb6d7c287b3544adcff601457d8a9b
shes50103 <shes50103@gmail.com>
2021-05-30 11:46:34 +0800

    sweetalert2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,21 +15,24 @@ ActiveStorage.start()
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import www from 'sweetalert2'
 
-Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire(
-      'Deleted!',
-      'Your file has been deleted.',
-      'success'
-    )
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  www.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
 })
