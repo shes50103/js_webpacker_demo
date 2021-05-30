0030e8b63627f552a36c4cd47c120dd84af8826f
shes50103 <shes50103@gmail.com>
2021-05-30 11:30:50 +0800

    bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,25 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
