c438e4ff8e6474cfe87757a02f28cd8fcec09769
shes50103 <shes50103@gmail.com>
2021-05-30 12:04:25 +0800

    done

---
+++ app/javascript/bootstrap/johnson.js
@@ -0,0 +1,14 @@
+console.log("hi")
+
+const a = 1
+
+export { a }
+
+const a2 = 12
+
+export { a2 }
+
+
+const b = 2
+
+export default b
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,24 +15,74 @@ ActiveStorage.start()
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import www from 'sweetalert2'
+// import www from 'sweetalert2'
 
 document.addEventListener("DOMContentLoaded", () => {
-  www.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Deleted!',
-        'Your file has been deleted.',
-        'success'
-      )
-    }
-  })
+  // www.fire({
+  //   title: 'Are you sure?',
+  //   text: "You won't be able to revert this!",
+  //   icon: 'warning',
+  //   showCancelButton: true,
+  //   confirmButtonColor: '#3085d6',
+  //   cancelButtonColor: '#d33',
+  //   confirmButtonText: 'Yes, delete it!'
+  // }).then((result) => {
+  //   if (result.isConfirmed) {
+  //     Swal.fire(
+  //       'Deleted!',
+  //       'Your file has been deleted.',
+  //       'success'
+  //     )
+  //   }
+  // })
 })
+
+// import bbbb, { a2 } from '../bootstrap/johnson'
+// console.log("a: ", a2)
+
+// // import bbbb from 'lib/johnson'
+// console.log("b: ", bbbb)
+
+// let aa = 1
+// window.w = aa
+
+
+// function 外面看不到裡面的變數
+// function test(){
+//   var a = 10
+// }
+
+// test()
+
+// console.log("a:", a)
+
+
+
+// function 裡面看的到外面的變數
+// var a = 10
+
+// function test(){
+//   console.log(a)
+// }
+
+// test()
+
+
+// var a = {}
+
+// function test(){
+//   var b = 10
+//   a.b = b
+// }
+
+// test()
+// console.log(a.b)
+
+// function test(){
+//   var b = 223
+//   window.b = b
+// }
+
+// test()
+// console.log(window.b)
+// console.log(b)
